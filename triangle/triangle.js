f4273764af6b92587bc7b7101bdd60fb20415ef3
Lum <supakorn.s@protonmail.com>
2024-02-13 21:21:00 +0700

    Fix vertex color

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -8,7 +8,9 @@ if (!gl) {
 let vertexShaderSource = `#version 300 es
 // An attribute is an input to a vertex shader. It will receive data from a buffer.
 in vec2 a_position;
+in vec3 a_color;
 uniform vec2 u_resolution;
+out vec3 v_color;
 
 void main() {
 	// gl_Position is a special variable a vertex shader is responsible for setting.
@@ -18,6 +20,7 @@ void main() {
 	vec2 zeroToOne = a_position / u_resolution;
 
 	gl_Position = vec4(a_position / u_resolution, 0, 1);
+	v_color = a_color; // Pass the colro to the fragment shader
 }
 `;
 
@@ -28,8 +31,10 @@ precision highp float;
 // Output from the fragment shader is the color
 out vec4 outColor;
 
+in vec3 v_color; // Input color from vertex shader
+
 void main() {
-	outColor = vec4(1, 0, 0.5, 1);
+	outColor = vec4(v_color, 1);
 }
 `;
 
@@ -69,6 +74,7 @@ let program = createProgram(gl, vertexShader, fragmentShader);
 // to this program
 // Look for a_position attribute in the program we just created
 let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+let colorAttributeLocation = gl.getAttribLocation(program, "a_color");
 let resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
 
 // Attributes get data from a buffer so we need to create that too.
@@ -76,12 +82,15 @@ let positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 // Put the data into the buffer by referencing the bind point
 let a = 400;
-let positions = [
+let vertices = [
 	0, a*Math.cos(Math.PI/6)/2,
+	1, 0, 0,
 	-a/2, -a*Math.cos(Math.PI/6)/2,
+	0, 1, 0,
 	a/2, -a*Math.cos(Math.PI/6)/2,
+	0, 0, 1
 ]
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
 // Set uniform
 gl.useProgram(program); // Set the current program
@@ -94,16 +103,19 @@ let vao = gl.createVertexArray();
 gl.bindVertexArray(vao); // Make it the current vertex array
 // Now we need to set up the attributes in the vertex array
 gl.enableVertexAttribArray(positionAttributeLocation);
+gl.enableVertexAttribArray(colorAttributeLocation);
 
 // Now we need to specify how to pull data out
 let size = 2;
 let type = gl.FLOAT
 let normalize = false;
-let stride = 0;
+let stride = 5 * Float32Array.BYTES_PER_ELEMENT;
 let offset = 0;
 // Also, the attribute is now bound to positionBuffer
 gl.vertexAttribPointer(
 	positionAttributeLocation, size, type, normalize, stride, offset);
+gl.vertexAttribPointer(colorAttributeLocation,
+	3, type, normalize, 5*Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
 
 // Tell WebGL how to convert from the clip space values we use to specify vertex position
 // to pixels (screen space)
